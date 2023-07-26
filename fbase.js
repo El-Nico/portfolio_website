4680bace1458b7de8f288fddc506aa4816699478
El-Nico <nicholasc1665@yahoo.com>
2023-07-26 17:55:57 +0100

    initialize project

---
+++ fbase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtdjhZKu9bF-WAjciEmmLUwkf7hquQmYY",
+  authDomain: "portfolio-website-v2-2023.firebaseapp.com",
+  projectId: "portfolio-website-v2-2023",
+  storageBucket: "portfolio-website-v2-2023.appspot.com",
+  messagingSenderId: "521951504046",
+  appId: "1:521951504046:web:efa201e777b75746f82f3f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
