7a2f64789388f05bf21df26f55169c726c86ae7b
El-Nico <nicholasc1665@yahoo.com>
2023-12-07 21:35:48 +0000

    code cleanup

--- functions/index.js
+++ functions/index.js
@@ -2,39 +2,37 @@ require("dotenv").config();
 const functions = require("firebase-functions");
 const nodeMailer = require("nodemailer");
 
-
 const runtimeOpts = {
   timeoutSeconds: 540,
 };
-exports.mailMe = functions.runWith(runtimeOpts)
-    .firestore
-    .document("messages/{messageId}")
+exports.mailMe = functions
+    .runWith(runtimeOpts)
+    .firestore.document("messages/{messageId}")
     .onCreate(async (snap, context) => {
       const newMessage = snap.data();
       const transporter = nodeMailer.createTransport({
-        //   host: "smtp.mail.yahoo.com",
-        //   port: 465,
+      //   host: "smtp.mail.yahoo.com",
+      //   port: 465,
         service: "yahoo",
         //   secure: true,
         auth: {
           user: process.env.USER,
           pass: process.env.PASS,
         },
-        //   debug: false,
-        //   logger: true,
+      //   debug: false,
+      //   logger: true,
       });
 
       /**
- * Solves equations of the form a * x = b
- * sends a mail to the provided destination
- * @param {object} message the message to be sent
- * from nodemailer transporter
- */
+     * sends a mail to the provided destination
+     * @param {object} message the message to be sent
+     * from nodemailer transporter
+     */
       async function sendMail(message) {
-        const info=await transporter.sendMail({
+        const info = await transporter.sendMail({
           from: process.env.USER,
           to: process.env.USER,
-          subject: message.subject, // Subject line
+          subject: `${message.subject} - FROM MY WEBSITE`, // Subject line
           text: message.message, // plain text body
           html: `<ul>
           <li>${message.name}</li>
@@ -44,7 +42,6 @@ exports.mailMe = functions.runWith(runtimeOpts)
         
         <hr />
         <p>${message.message}</p>`, // html body
-
         });
         console.log("Message sent: %s", info.messageId);
       }
--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -3,7 +3,6 @@ import { Firestore, collection } from '@angular/fire/firestore';
 import { FormBuilder, Validators } from '@angular/forms';
 import AOS from "aos";
 import { CollectionReference, DocumentReference, addDoc } from 'firebase/firestore';
-import { Observable } from 'rxjs';
 import { Message } from 'src/app/models/models';
 
 @Component({
@@ -18,9 +17,9 @@ export class ContactComponent {
   private firestore: Firestore = inject(Firestore);
   messagesCollection: CollectionReference;
 
-  
+
   isSubmitted = false;
-  sending=false;
+  sending = false;
   contactForm = this.fb.group({
     name: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
@@ -28,37 +27,37 @@ export class ContactComponent {
     phone: ['', Validators.pattern(/^[\+\(\s.\-\/\d\)]{5,30}$/)],
     message: ['', Validators.required]
   })
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder) {
     this.messagesCollection = collection(this.firestore, 'messages')
     AOS.init();
   }
 
-  addMessage(message:Message){
-  return addDoc(this.messagesCollection, <Message> message)
+  addMessage(message: Message) {
+    return addDoc(this.messagesCollection, <Message>message)
   }
 
   checkInvalid(formControl: string): boolean {
     //this is more error specific and can provide targeted error message to span
     // return (this.contactForm.get(formControl)?.hasError('required') &&
-    return ((this.contactForm.get(formControl)?.invalid && 
-          ((this.contactForm.get(formControl)?.dirty || this.contactForm.get(formControl)?.touched) || this.isSubmitted)) 
-    || false)
+    return ((this.contactForm.get(formControl)?.invalid &&
+      ((this.contactForm.get(formControl)?.dirty || this.contactForm.get(formControl)?.touched) || this.isSubmitted))
+      || false)
   }
 
   onSubmit() {
     this.isSubmitted = true
-    if(!this.contactForm.valid){
+    if (!this.contactForm.valid) {
       // alert('invalid field in form, pls check again')
       return
     }
-    const message=this.contactForm.value as Message
-  this.sending=true;
-    this.addMessage(message).then((documentReference:DocumentReference)=>{
+    const message = this.contactForm.value as Message
+    this.sending = true;
+    this.addMessage(message).then((documentReference: DocumentReference) => {
 
-this.sending=false;
-this.isSubmitted=false;
-this.contactForm.reset();
-alert('message sent, thanks!')
+      this.sending = false;
+      this.isSubmitted = false;
+      this.contactForm.reset();
+      alert('message sent, thanks!')
     })
   }
 }
